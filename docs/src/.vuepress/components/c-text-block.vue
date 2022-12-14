f5555a7384ae4cabd2612f7e8de589ddf4b85b6a
npnpnpnpnpnp <19845311+npnpnpnpnpnp@users.noreply.github.com>
2022-12-14 17:53:00 +0100

    Tidy up components

---
+++ docs/src/.vuepress/components/c-text-block.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="component">
+    <div v-html="item"/>
+    <!-- <c-base-bodytext :class="$style.body" v-html="item" /> -->
+  </section>
+</template>
+
+<script>
+// import CBaseBodytext from "@/components/c-base-bodytext";
+
+export default {
+  // components: { CBaseBodytext },
+  props: {
+    item: {
+      type: String,
+      required: true,
+      default: ""
+    },
+    type: {
+      type: String,
+      default: 'info'
+    }
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    classes() {
+      return {
+        component: [this.$style.component]
+      };
+    }
+  },
+
+};
+</script>
+
