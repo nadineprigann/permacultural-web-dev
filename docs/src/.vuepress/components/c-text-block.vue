15c3c6b5d9420da6405087569ed78b5e62a5496e
npnpnpnpnpnp <19845311+npnpnpnpnpnp@users.noreply.github.com>
2023-01-11 23:42:30 +0100

    Revert "Revert "Define basic styling for custom block""
    This reverts commit ec3995905428a129db7d75ec051d5e2277a164fb.

--- docs/src/.vuepress/components/c-text-block.vue
+++ docs/src/.vuepress/components/c-text-block.vue
@@ -1,6 +1,7 @@
 <template>
   <section class="component">
-    <div v-html="text"/>
+    <div v-html="type" class="type"/>
+    <div v-html="text" class="text"/>
     <!-- <c-base-bodytext :class="$style.body" v-html="item" /> -->
   </section>
 </template>
@@ -35,3 +36,26 @@ export default {
 };
 </script>
 
+<style scoped lang="css">
+.component {
+  --highlight-color: rgb(66, 185, 131);
+  /* --bg: rgb(113, 168, 255); */
+  background-color: rgb(45, 45, 45);
+  border-left: solid 0.5rem var(--highlight-color);
+  padding: 1em 1.2em;
+  margin: 2em 0;
+}
+
+.type,
+.text {
+  font-size: 0.85em;
+}
+
+.type {
+  text-transform: uppercase;
+  letter-spacing: 0.2em;
+  margin-bottom: 1.25em;
+  color: var(--highlight-color);
+  font-weight: 600;
+}
+</style>
