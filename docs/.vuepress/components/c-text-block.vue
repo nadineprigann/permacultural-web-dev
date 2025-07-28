e3de37dda5cd794ad43b161ac43e1ec5881a0cae
npnpnpnpnpnp <19845311+npnpnpnpnpnp@users.noreply.github.com>
2025-07-28 15:06:43 +0200

    Use light mode to level with website style

--- docs/.vuepress/components/c-text-block.vue
+++ docs/.vuepress/components/c-text-block.vue
@@ -39,24 +39,24 @@ export default {
 <style scoped lang="css">
 .block {
   /* --bg: rgb(113, 168, 255); */
-  background-color: rgb(45, 45, 45);
+  background-color: #ebebebff;
   border-left: solid 0.5rem var(--highlight-color);
   padding: 1em 1.2em;
   margin: 2em 0;
 
   &.label-succession {
-    --highlight-color: rgb(66, 185, 131);
+    --highlight-color: #2c855d;
   }
 
   &.label-resource {
-    --highlight-color: rgb(252, 185, 108);
+    --highlight-color: #d36e00;
   }
 
   &.label-feature {
-    --highlight-color: rgb(228, 164, 255);
+    --highlight-color: #9a01ff;
   }
   &.label-info {
-    --highlight-color: rgb(141, 221, 255);
+    --highlight-color: #0c5cb8;
   }
 }
 
